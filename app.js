4f0d759bb06c5f47ace4eef9e98563e3b78eb865
Kristina <you@example.com>
2023-04-22 13:46:35 -0500

    json

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+app.get('/',  (req, res) => {
+
+  res.send("hello world from express!")
+ 
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running & listening on port ${PORT}`);
+});
